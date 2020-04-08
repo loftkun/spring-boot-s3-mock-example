c612565910ac267699f5a581cbb2405227f3a9f5
loftkun <loftkun@gmail.com>
2020-04-09 00:31:40 +0900

    add test with mockito

--- src/main/java/com/example/loft/S3Example.java
+++ src/main/java/com/example/loft/S3Example.java
@@ -1,12 +1,11 @@
 package com.example.loft;
 
-import com.amazonaws.AmazonServiceException;
-import com.amazonaws.SdkClientException;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.client.builder.AwsClientBuilder.EndpointConfiguration;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.Bucket;
 import com.amazonaws.services.s3.model.CreateBucketRequest;
 import com.amazonaws.services.s3.model.GetBucketLocationRequest;
 
@@ -22,16 +21,29 @@ public class S3Example {
 
     private static final Logger logger = LoggerFactory.getLogger(S3Example.class);
 
-    public void createBucket(String bucketName){
-        AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
+    private AmazonS3 client = null;
+
+    public S3Example(AmazonS3 client) {
+        this.client = client;
+	}
+
+	public boolean createBucket(String bucketName){
+        if ( this.client == null ){
+            client = AmazonS3ClientBuilder.standard()
                 .withEndpointConfiguration(new EndpointConfiguration(endpoint, region))
                 .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey, secretAccessKey)))
                 .build();
-        if (s3Client.doesBucketExistV2(bucketName)) {
-            logger.info("doesBucketExistV2");
-            return;
         }
-        s3Client.createBucket(new CreateBucketRequest(bucketName));
-        logger.info("Bucket location: " + s3Client.getBucketLocation(new GetBucketLocationRequest(bucketName)));
+        if (client.doesBucketExistV2(bucketName)) {
+            logger.info("doesBucketExistV2");
+            return false;
+        }
+        //Bucket bucket = client.createBucket(new CreateBucketRequest(bucketName));
+        Bucket bucket = client.createBucket(bucketName);
+        logger.info(bucket.toString());
+
+        //return client.getBucketLocation(new GetBucketLocationRequest(bucketName));
+        logger.info(client.getBucketLocation(bucketName));
+        return true;
     }
 }
